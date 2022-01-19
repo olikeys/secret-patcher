dbc72ef6307875bff1e42133271ff478daeae96e
Oli Keys <oli.keys@snyk.io>
2022-01-19 12:54:07 +0000

    testing

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+)
+
+func main() {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// get secrets from default
+
+	secret, err := clientset.CoreV1().Secret("test-secret")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Secret is %s", secret)
